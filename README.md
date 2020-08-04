fdb5b4cfff29d731b1d0eeecb118e9ff983b53c7
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-08-04 13:28:47 -0700

    main commit and publish readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# face-recognition-app
+# Face Recognition
+
+## Summary
+Small application that uses the Yalefaces databases to train the AI. The realtime version uses the Local Binary Patterns algorithm and works on video.
+
+## Tech Stack
+- Python
+- Numpy
+- OpenCV
+- Argparse
+- Time
---
+++ realtime.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+
+from dataset import load_data
+
+# create face detector and load data
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+(X_train, y_train), (X_test, y_test) = load_data(face_cascade)
+
+# create and train face recognizer
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+face_recognizer.train(X_train, np.array(y_train))
+
+# open webcam
+video_cap = cv2.VideoCapture(0)
+while True:
+    # get frame form webcam
+    _, frame = video_cap.read()
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # run face detection
+    faces = face_cascade.detectMultiScale(grey)
+    for face in faces:
+        # extract face from frame
+        x, y, w, h = face
+        face_region = grey[y:y+h, x:x+w]
+        
+        # run face recognition
+        predicted_person, _ = face_recognizer.predict(face_region)
+        
+        # draw rectangle over video
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
+        cv2.putText(frame, str(predicted_person), (x, y), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 255))
+        
+    # show annotated frame
+    cv2.imshow('face recognition', frame)
+    
+    # quit app
+    if cv2.waitKey(1) == ord('q'):
+        break
+    # end while loop
+
+# close webcam and release resources
+video_cap.release()
+cv2.destroyAllWindows()
